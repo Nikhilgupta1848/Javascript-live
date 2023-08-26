fd0b5f4099e5982228876e462b981d845af88975
Nikhilgupta1848 <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-08-26 07:26:15 +0000

    Coversion of Datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "nik"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33"=> 33
+// "33abc" ,null,undefined = NaN
+// boolean - true=>1 , false => 0
+
+let isLoggedIn = "nik"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" = false
+// "nikhil" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
