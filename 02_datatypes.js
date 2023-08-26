8842bcf867a6fa1065c9a5d54f6021f8c1992a23
Nikhilgupta1848 <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-08-26 07:06:05 +0000

    variables

---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as newer version
+
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+// code readability that should be high 
+
+ let name = "nikhil"
+ let age = 18
+ let isLoggedIn = false
+
+
+ // number => 2 to pow 53
+ // bigint 
+ // string => ""
+ // boolean => true /false
+ // null => standalone value 
+ //undefined => 
+ //symbol => unique
+
+
+
+ // object 
+
+ console.log(typeof undefined);// undefined
+ console.log(typeof null); // object
