08efb5114826bc4806125157a32342cc9d2a0ba8
Nikhilgupta1848 <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-08-26 07:06:49 +0000

    variables

--- 01_variable.js
+++ 01_variable.js
@@ -2,7 +2,17 @@ const accountId  = 144553
 let accountEmail = "nikhilkrkanti@gmail.com"
 var accountCity = "12345"
 accountCity = "jaipur"
-
+let accountState;
 // accountId = 2
 
+accountEmail = "nik@gmail.comn"
+accontPassword = "212121"
+accountCity = "bengaluru"
+
 console.log(accountId);
+
+/*Note- prefer not to use var
+ because of issue in block scope and functional scope
+
+*/
+console.table([accountEmail,accountId,accontPassword,accountCity,accountState])
