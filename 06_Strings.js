4c35cc9078c616b7883d29e08fb26b5dcf98ac11
Nikhil Kumar <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-09-01 11:56:58 +0000

    Strings

---
+++ 06_Strings.js
@@ -0,0 +1,40 @@
+ const name = "Nikhil"
+
+// const repCount = 5
+
+ // console.log(name + repCount + " Value"); not recommended
+
+// console.log(`hello my name is ${name} and my repo count is ${repCount}`);
+
+ const gameName = new String ("Nikhil-nik-50")
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+//  const newString = gameName.substring(0,4)
+
+//  console.log(newString);
+
+//  const anotherString = gameName.slice(-5,4 )
+//  console.log(anotherString);
+
+// const newStringOne = "   Nikhil   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://nikhil.com/nik%20kumar"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('niku'));
+
+console.log(gameName.split('-'));
