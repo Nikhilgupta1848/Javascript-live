e1a30674631acaf77c3b8675af0cbe47dae87b7b
Nikhil Kumar <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-09-20 10:37:43 +0000

    array2

---
+++ .vscode/10_arrays2.js
@@ -0,0 +1,37 @@
+const  names = ["nik" , "muk" ,"pranj" , "pratham"]
+const lang = ["js" , "c" , "php", "java"]
+
+//  names.push(lang)
+
+//  console.log(names);
+//  console.log(names[4][1]);
+
+// const namesandlang = names.concat(lang)
+// console.log(namesandlang);
+
+// const namesandandlang = [...names, ...lang]
+
+// console.log(namesandandlang);
+
+// const nos = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const extrarray = nos.flat(Infinity)
+
+// console.log(extrarray);
+
+
+
+
+
+
+
+console.log(Array.isArray("NIKHil"));
+console.log(Array.from("Nikhil"));
+console.log(Array.from({name: "hitesh"})); //interesting imp
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
