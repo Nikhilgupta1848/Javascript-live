4fba43e91e8474cbb1f076aee923515ddcf9b2e8
Nikhil Kumar <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-09-27 06:44:21 +0000

    Objects

---
+++ .vscode/11_objects.js
@@ -0,0 +1,40 @@
+// Singleton
+
+
+
+// Object.create
+
+
+// Object literals 
+
+const mySym = Symbol("Key1")
+
+const JsUser = {
+    name : "Nikhil",
+    "full name" : "Nikhil Kumar",
+     [mySym] : "Mykey1",
+    age : 22,
+    Location : "Bangalore",
+    email : "nikhilkrkanti@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays : ["Monday", "Sunday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log( JsUser[mySym]);
+
+JsUser.email = "Nikhilkr9204@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email = "Nikhilkr9205@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello Js user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`hello Js user, ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
