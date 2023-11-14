3851242632c3eca16910dc7eaf4c520b0f21faad
Nikhil Kumar <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-11-14 10:37:51 +0000

    scopes

---
+++ .vscode/14_scopes.js
@@ -0,0 +1,20 @@
+// var  c = 300
+let a = 300
+if (true) {
+    let a = 10 
+    const b = 20
+  console.log("INNER : ", a);
+  function addnum(){
+
+    
+  }
+}
+
+
+    
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
--- .vscode/tempCodeRunnerFile.js
+++ .vscode/tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-
-// console.log(returnSecondvalue(myNewArray));
+console.log(b);
