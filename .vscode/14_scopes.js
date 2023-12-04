7586271cd0053dbb29e09753aa592c9093a96ec5
Nikhil Kumar <69416221+Nikhilgupta1848@users.noreply.github.com>
2023-12-04 16:26:24 +0000

    scopes completed

--- .vscode/14_scopes.js
+++ .vscode/14_scopes.js
@@ -1,20 +1,56 @@
-// var  c = 300
-let a = 300
-if (true) {
-    let a = 10 
-    const b = 20
-  console.log("INNER : ", a);
-  function addnum(){
+ // var  c = 300
+//  let a = 300
+//  if (true) {
+//     let a = 10 
+//     const b = 20
+//  console.log("INNER : ", a);
+//    function addnum(){
+
+
+//    }
+//  }
+
 
     
-  }
+
+
+
+// function one (){
+//     const username = "Nikhil"
+
+//     function two(){
+//      const website = "youtube"
+//      console.log(username);
+//    }
+     // console.log(website);
+
+//      two()
+
+
+
+// }
+
+// one()
+// if(true) {
+//   const username = "Nikhil"
+//   if (username === "Nikhil") {
+//     const website = " youtube"
+//     console.log(username + website );
+//   }
+  // console.log(website);
+// }
+// console.log(username);
+
+// ******************* interesting ****************//
+
+console.log(addone (5)); 
+function addone(num) {
+  return num + 1
+  
 }
 
-
-    
-
-
-console.log(a);
-// console.log(b);
-// console.log(c);
+addTwo(5)
+const addTwo = function (num) {
+  return num + 2
+}
 
--- .vscode/tempCodeRunnerFile.js
+++ .vscode/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-console.log(b);
+two()
